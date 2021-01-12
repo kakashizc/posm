133d4627166dc4b298d5091601d24a335a262f55
kakashizc <359005442@qq.com>
2021-01-12 20:38:07 +0800

    123

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -11,27 +11,42 @@ class Index extends Api
 {
     protected $noNeedLogin = ['*'];
     protected $noNeedRight = ['*'];
+    private $_rsa;
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->_rsa = new Rsa();
+    }
 
     /**
-     * 首页123
+     * 首页
      *
      */
     public function index()
     {
-        $rsa = new Rsa();
+
         $data['name'] = 'Tom';
         $data['age']  = '20';
-//        $privEncrypt = $rsa->privEncrypt(json_encode($data));
-//        echo '私钥加密:'.$privEncrypt.'<br>';
+        $privEncrypt = $this->_rsa->privEncrypt(json_encode($data));
+        echo '私钥加密:'.$privEncrypt.'<br>';
 //
-//        $publicDecrypt = $rsa->publicDecrypt($privEncrypt);
-//        echo '公钥解密:'.$publicDecrypt.'<br>';
+        $publicDecrypt = $this->_rsa->publicDecrypt($privEncrypt);
+        echo '公钥解密:'.$publicDecrypt.'<br>';
 
-        $publicEncrypt = $rsa->publicEncrypt(json_encode($data));
+        $publicEncrypt = $this->_rsa->publicEncrypt(json_encode($data));
         echo '公钥加密:'.$publicEncrypt.'<br>';
 
-        $privDecrypt = $rsa->privDecrypt($publicEncrypt);
+        $privDecrypt = $this->_rsa->privDecrypt($publicEncrypt);
         echo '私钥解密:'.$privDecrypt.'<br>';
+    }
 
+    /*
+     * 加解密测试
+     * */
+    public function test()
+    {
+        $data = $this->request->param('pass');
+        $publicDecrypt = $this->_rsa->publicDecrypt($data);
+        echo '公钥解密:'.$publicDecrypt.'<br>';
     }
 }

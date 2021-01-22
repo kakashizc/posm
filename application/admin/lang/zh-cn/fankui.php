01cec9528ab3cabcf70b6179c1c280eed3ebc3a4
kakashizc <359005442@qq.com>
2021-01-22 13:16:41 +0800

    avatar

---
+++ application/admin/lang/zh-cn/fankui.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Cont'   => '反馈内容',
+    'Mobile' => '手机号'
+];
---
+++ public/assets/js/backend/fankui.js
@@ -0,0 +1,50 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'fankui/index' + location.search,
+                    add_url: 'fankui/add',
+                    edit_url: 'fankui/edit',
+                    del_url: 'fankui/del',
+                    multi_url: 'fankui/multi',
+                    table: 'fankui',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'mobile', title: __('Mobile')},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});

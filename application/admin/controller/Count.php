4efb1e4859a488f81eec7e5d3d5777d62e3361d3
kakashizc <359005442@qq.com>
2021-02-23 11:27:00 +0800

    123

--- application/admin/controller/Count.php
+++ application/admin/controller/Count.php
@@ -22,7 +22,7 @@ class Count extends Backend
         $totals = Db::name('sn_record')->whereTime('ctime',[$stime,$etime])->sum('money');
         $records = Db::name('sn_record')->whereTime('ctime',[$stime,$etime])->count('id');
         $users = Db::name('auser')->whereTime('ctime',[$stime,$etime])->count();
-        $this->assign('totals',$totals);//刷卡总额(万元)
+        $this->assign('totals',$totals/10000);//刷卡总额(万元)
         $this->assign('users',$users);//用户总数
         $this->assign('records',$records);//刷卡记录总数
         return $this->view->fetch();
